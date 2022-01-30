995edbe0849f8e046ed78f099f653230bb1a4d52
toscap2002 <95303629+toscap2002@users.noreply.github.com>
2022-01-31 00:22:54 +0100

    Update AppConfig.java

--- src/main/java/com/example/Weather/config/AppConfig.java
+++ src/main/java/com/example/Weather/config/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * @author Pierro e Pelliccia
+ * @author Pierro
  * Definisce il RestTemplete @bean che viene @autowired
  */
 
